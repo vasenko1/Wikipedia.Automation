43c7a1f8549cc6baaef33bdad1e0d7b97e6870d6
igor.va@sbtech.com <Universe85>
2017-11-30 18:21:40 +0200

    First Code commit

---
+++ Pages/BasePage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace Wikipedia.Automation.Pages
+{
+    class BasePage
+    {
+        protected IWebDriver Driver { get; set; }
+
+        private static int DefaultTimeoutInSeconds = 10;
+
+        public string GetCurrentUrl()
+        {
+            return Driver.Url;
+        }
+
+        public void GoToPageUrl(string reportUrl, string title)
+        {
+            var _domain = new Uri(Driver.Url).GetComponents(UriComponents.SchemeAndServer, UriFormat.Unescaped);
+            var url = _domain + "/" + reportUrl;
+
+            Driver.Navigate().GoToUrl(url);
+
+            new WebDriverWait(Driver, TimeSpan.FromSeconds(20))
+                .Until(ExpectedConditions.TitleIs(title));
+        }
+
+        public void WaitForElementPresent(string cssElement, int sec)
+        {
+            new WebDriverWait(Driver, TimeSpan.FromSeconds(sec))
+                .Until(ExpectedConditions.ElementIsVisible(By.CssSelector(cssElement)));
+        }
+    }
+}
---
+++ Pages/MainPage.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Wikipedia.Automation.Pages
+{
+    class MainPage : BasePage
+    {
+        [FindsBy(How = How.Id, Using = "js-link-box-en")]
+        private IWebElement _englishPageLink;
+
+        [FindsBy(How = How.Id, Using = "js-link-box-ru")]
+        private IWebElement _russianPageLink;
+
+        [FindsBy(How = How.ClassName, Using = "pure-button pure-button-primary-progressive")]
+        private IWebElement _searchField;
+
+        [FindsBy(How = How.Id, Using = "searchLanguage")]
+        private IWebElement _searchLanguageDropdown;
+
+        [FindsBy(How = How.Id, Using = "js-lang-list-button")]
+        private IWebElement _selectLanguageMenuButton;
+
+        public HomeDashboardPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+
+        
+        public void CheckUserIsOnMainPage()
+        {
+            Assert.IsTrue(_selectLanguageMenuButton.Displayed, "Error, User is not at Login Page!");
+        }
+
+    }
+}
